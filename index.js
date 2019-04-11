b2cde7aa02426bfefe2c467412301906a324c5b0
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-11 16:07:21 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function createPost(){
+    let title = document.getElementById('postTitle').value;
+    let post = document.getElementById('postText').value;
+    let author = document.getElementById('postAuthor').value;
+
+    let pageTemplateFn = _.template(document.getElementById("page-template").innerHTML);
+    let postTemplateFn = _.template(document.getElementById("post-template").innerHTML);
+    let commentsTemplateFn = _.template(document.getElementById("comments-template").innerHTML);
+
+    let main = document.getElementsByTagName('main')[0];
+    main.innerHTML += pageTemplateFn({post:postTemplateFn({post_title: title, post_body: post, post_author: author})});
+
+    let divPost = document.getElementById('post');
+    let commentsSection = commentsTemplateFn();
+
+    divPost.innerHTML += commentsSection;
+
+}
+
+function postComment(){
+
+    let commenter = document.getElementById('commenterName').value;
+    let comment = document.getElementById('commentText').value;
+
+    let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+    let commentSection = commentTemplate({commenter: commenter, comment: comment});
+
+    let divComments = document.getElementById('comments');
+    divComments.innerHTML += commentSection;
+}
